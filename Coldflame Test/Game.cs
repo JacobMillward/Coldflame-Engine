1174c36f4f6a91e70406dc8ba34707542acb829a
Jacob Millward <jacob.millward@gmail.com>
2015-06-19 21:50:18 +0100

    Move non-library code into ColdFlame Test project

---
+++ Coldflame Test/Game.cs
@@ -0,0 +1,18 @@
+﻿using ColdFlame;
+
+namespace Coldflame_Test
+{
+    class Game : ColdFlame.GameBase
+    {
+        protected override void Initialise()
+        {
+            SystemList.Add(new RenderSystem(new SFML.System.Vector2u(800, 600)));
+            SystemList.Add(new AnimationSystem());
+
+            Entity player = new Entity();
+            player.AddComponent(new Position(0, 0));
+            player.AddComponent(new Sprite(@"player.png"));
+            player.AddComponent(new Animation(@"player.png", 24, 25, 6));
+        }
+    }
+}
